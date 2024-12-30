d4e7c0967eadc8f90f2261e534929b2c2e7f29f8
Alessandro Campolo <campoloalex@gmail.com>
2024-12-30 19:16:17 +0100

    weather configuration

--- modules/weather.py
+++ modules/weather.py
@@ -1,16 +1,66 @@
-from datetime import datetime
 
 from basemod import BaseModule
 
 import requests
+from urllib.parse import quote
+
 
 class Weather(BaseModule):
-    def __init__(self, **kwargs):
+    def __init__(self, location:str, language:str=None,
+                 narrow=False, metric:bool|None=None, speed_in_m_s=False,
+                 today_forecast=False, tomorrow_forecast=False,
+                 quiet=True, show_city=False, no_colors=False, console_glyphs=False,
+                 **kwargs):
+        self.location = quote(location)
+        self.language = language
+        self.narrow = narrow
+        self.metric = metric
+        self.speed_in_m_s = speed_in_m_s
+        self.today_forecast = today_forecast
+        self.tomorrow_forecast = tomorrow_forecast
+        self.quiet = quiet
+        self.show_city = show_city
+        self.no_colors = no_colors
+        self.console_glyphs = console_glyphs
+        
+        self.url = 'https://wttr.in/' + self.location + '?AF'
+        if today_forecast and tomorrow_forecast:
+            self.url += '2'         # current weather + today's + tomorrow's forecastc
+        elif today_forecast:
+            self.url += '1'         # current weather + today's forecast
+        else:
+            self.url += '0'         # only current weather
+        
+        if today_forecast and narrow:
+            self.url += 'n'         # narrow version (only day and night)
+        
+        if quiet and not show_city:
+            self.url += 'Q'         # superquiet version (no "Weather report", no city name)
+        elif quiet:
+            self.url += 'q'         # quiet version (no "Weather report" text)
+        
+        if no_colors:
+            self.url += 'T'         # switch terminal sequences off (no colors)
+            
+        if console_glyphs:
+            self.url += 'd'         # restrict output to standard console font glyphs
+        
+        if metric is True:
+            self.url += 'm'         # metric (SI) (used by default everywhere except US)
+        elif metric is False:
+            self.url += 'u'         # USCS (used by default in US)
+            
+        if speed_in_m_s:
+            self.url += 'M'         # show wind speed in m/s
+        
+        self.headers = {'User-Agent':'curl'}
+        if language:
+            self.headers['Accept-Language'] = language
+        
         super().__init__(**kwargs)
         
+        
     def __call__(self):
-        t=requests.get('https://wttr.in/Reggio+Calabria?0AQd&lang=it', headers={'User-Agent':'curl'}).text
-        # t = '\n'.join(t.splitlines()[2:7])
-        return t
+        return requests.get(self.url, headers=self.headers).text
     
 widget = Weather
