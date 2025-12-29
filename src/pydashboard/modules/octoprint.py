a3c881f190ca872ee52d93175b1aef04c251f739
Alessandro Campolo <campoloalex@gmail.com>
2025-12-29 13:17:56 +0100

    Fixed octoprint string generation

--- src/pydashboard/modules/octoprint.py
+++ src/pydashboard/modules/octoprint.py
@@ -1,8 +1,10 @@
 from typing import Any
 
 from octorest import OctoRest
+from requests.packages import target
 
 from pydashboard.containers import BaseModule
+from pydashboard.utils.units import duration_fmt
 
 
 class OctoPrint(BaseModule):
@@ -36,27 +38,41 @@ class OctoPrint(BaseModule):
             out = ''
             client = OctoRest(url=self.url, apikey=self.token)
             job_info = client.job_info()
-            out += 'State:', job_info['state'] + '\n'
-            out += 'File:', job_info['job']['file']['name'] + '\n'
-            if job_info['progress']['completion'] is not None:
-                out += 'Progress: {:.3f}%'.format(job_info['progress']['completion']) + '\n'
-            if job_info['progress']['printTime'] is not None:
-                out += 'Print time: {}s'.format(job_info['progress']['printTime']) + '\n'
-            if job_info['progress']['printTimeLeft'] is not None:
-                out += 'Time left: {}s'.format(job_info['progress']['printTimeLeft']) + '\n'
+            out += f"State: {job_info['state']}\n"
+            out += f"File: {job_info['job']['file']['name']}\n"
+            if (completion:=job_info.get('progress', {}).get('completion')) is not None:
+                out += f'Progress: {completion:.3f}%' + '\n'
+            if (printTime:=job_info.get('progress', {}).get('printTime')) is not None:
+                out += f'Print time: {duration_fmt(printTime)}s' + '\n'
+            if (printTimeLeft:=job_info.get('progress', {}).get('printTimeLeft')) is not None:
+                out += f'Time left: {duration_fmt(printTimeLeft)}s' + '\n'
 
             conn_info = client.connection_info()
-            self.border_subtitle = conn_info['current']['state']
-            self.styles.border_subtitle_color = 'green'
-            if conn_info['current']['port'] is not None:
+            b_sub = conn_info.get('current', {}).get('state', 'N/A')
+            self.border_subtitle = b_sub
+            self.styles.border_subtitle_color = 'green' if b_sub != 'N/A' else 'red'
+            if conn_info.get('current', {}).get('port') is not None:
                 printer = client.printer()
-                max_len = max([len(x) for x in printer['temperature'].keys()])
-                out += 'Temperatures:' + '\n'
-                for tool, temp in printer['temperature'].items():
-                    if temp['target']:
-                        out += ' ', tool.ljust(max_len), f"{temp['actual']:.1f}°C/{temp['target']:.1f}°C" + '\n'
-                    else:
-                        out += ' ', tool.ljust(max_len), f"{temp['actual']:.1f}°C/off" + '\n'
+                temperatures = printer.get('temperature', {})
+                if temperatures:
+                    max_len = max([len(x) for x in temperatures.keys()])
+                    out += 'Temperatures:' + '\n'
+                    for tool, temp in temperatures.items():
+                        if not temp.get('actual') and not temp.get('offset') and not temp.get('target'):
+                            #Exclude empty sensors
+                            continue
+                        out += ' ' + tool.ljust(max_len) + ' '
+                        if (actual:=temp.get('actual')) is not None:
+                            out += f"{actual:.1f}°C"
+                        else:
+                            out += 'N/A'
+                        if (target:=temp.get('target')) is not None:
+                            if target:
+                                out += f"/{target:.1f}°C\n"
+                            else:
+                                out += '/off\n'
+                        else:
+                            out += '/N/A\n'
 
             return out
 
